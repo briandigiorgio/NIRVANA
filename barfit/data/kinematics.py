c91284fdd43470b3a21c0da038d009e9322bd51c
Kyle Westfall <westfall@ucolick.org>
2020-09-28 16:11:11 -0700

    minor

--- barfit/data/kinematics.py
+++ barfit/data/kinematics.py
@@ -338,14 +338,6 @@ class Kinematics(FitArgs):
 
         return _data, _ivar, _mask
 
-#    def remap_data(self, data, masked=False):
-#        if data.shape != self.vel.shape:
-#            raise ValueError('To remap, must have the same shape as the internal data attributes.')
-#        _data = np.ma.masked_all(self.spatial_shape, dtype=float) \
-#                    if masked else np.zeros(self.spatial_shape, dtype=float)
-#        _data[np.unravel_index(self.grid_indx, self.spatial_shape)] = data[self.bin_inverse]
-#        return _data
-
     def remap(self, data, masked=True):
         """
         Remap the requested attribute to the full 2D array.

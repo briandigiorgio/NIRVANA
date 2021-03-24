1e567ab28a0570994cdf068eb7a484240d3e2a26
Brian DiGiorgio <bdigiorg@ucsc.edu>
2021-03-23 18:03:43 -0700

    Bump version: 0.1.0 → 0.1.1dev

--- docs/conf.py
+++ docs/conf.py
@@ -22,9 +22,9 @@ copyright = '2021, Brian DiGiorgio, Kyle Westfall'
 author = 'Brian DiGiorgio, Kyle Westfall'
 
 # The short X.Y version.
-version = '0.1.0'
+version = '0.1.1dev'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1dev'
 
 
 # -- General configuration ---------------------------------------------------
--- nirvana/__init__.py
+++ nirvana/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = '0.1.0'
+__version__ = '0.1.1dev'
 __license__ = 'BSD3'
 __author__ = 'Brian DiGiorgio'
 __maintainer__ = 'Brian DiGiorgio'
